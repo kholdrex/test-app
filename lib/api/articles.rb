860ee3eb1713c7b898746e3a8eb3bc947af6c8d9
kholdrex <alexandrkholodniak@gmail.com>
2016-08-24 20:34:52 +0300

    fix api & swagger

--- lib/api/articles.rb
+++ lib/api/articles.rb
@@ -2,16 +2,33 @@ module API
   class Articles < Grape::API
     format :json
 
+    helpers do
+      def empty_content
+        {}
+      end
+
+      def not_found!
+        error!('Not found', 404)
+      end
+
+    end
+
     resource :articles do
 
       desc "GET articles", entity: API::Entities::Articles
       get do
-        present Article.all
+        present Article.all, with: API::Entities::Articles
       end
 
       desc "GET article by Id"
-      get :id do
-        present Article.find_by(id: params[:id])
+      route_param :id do
+        get do
+          article = Article.find_by(id: params[:id])
+
+          not_found! unless article
+
+          present article, with: API::Entities::Articles
+        end
       end
     end
   end
